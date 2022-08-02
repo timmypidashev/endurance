e835dfa4e88230a54c02a3250aa7b91f04bd3027
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-01 20:04:06 -0700

    Initial project structure

---
+++ endurance/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "endurance"
+__description__ = "Chromebook modification power-tool."
+__author__ = "Timothy Pidashev"
+__email__ = "pidashev.tim@gmail.com"
+__copyright__ = "Copyright 2022-Present, Timothy Pidashev"
+__license__ = "MIT"
+__version__ = "0.1.0-rc0"
+__status__ = "Prototype"
+
+def run_as_module():
+    print(__description__)
---
+++ endurance/__main__.py
@@ -0,0 +1,4 @@
+import endurance
+
+if __name__ == "__main__":
+    endurance.run_as_module()
