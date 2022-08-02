f40e7346635520728bf240d7bbd8a3b250fe093a
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-01 21:04:08 -0700

    Bump version

--- endurance/__init__.py
+++ endurance/__init__.py
@@ -4,7 +4,7 @@ __author__ = "Timothy Pidashev"
 __email__ = "pidashev.tim@gmail.com"
 __copyright__ = "Copyright 2022-Present, Timothy Pidashev"
 __license__ = "MIT"
-__version__ = "0.1.0-rc1"
+__version__ = "0.1.0"
 __status__ = "Prototype"
 
 import argparse
